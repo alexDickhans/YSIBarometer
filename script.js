328b62327520040f1ed42b62e9b800e9efb8d1e1
alexDickhans <55064247+alexDickhans@users.noreply.github.com>
2023-11-08 23:45:15 +0000

    Converted barometric pressure to int

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ function submitString() {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputString}&appid=${apiKey}`)
     .then(response => response.json())
     .then(data => {
-      const barometricPressure = parseFloat(data.main.pressure) * 0.75006 - 0.025*altitude;
+      let barometricPressure = parseFloat(data.main.pressure) * 0.75006 - 0.025*altitude;
       
+      barometricPressure = parseInt(barometricPressure);
       document.getElementById('barometric_output').innerText = barometricPressure;
     })
     .catch(error => {
