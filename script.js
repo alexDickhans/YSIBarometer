84671711a852045c33e1f1bfba11b41dd477c6d4
AlexDickhans <7950170-AlexDickhans@users.noreply.replit.com>
2023-11-08 23:40:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const apiKey = '20d0b74210e3ef793c0b63567f587492';
+
+window.onload = function() {
+  submitString();
+};
+
+function submitString() {
+  const inputString = document.getElementById('inputString').value;
+  const altitude = 5000;
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputString}&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      const barometricPressure = parseFloat(data.main.pressure) * 0.75006 - 0.025*altitude;
+      
+      document.getElementById('barometric_output').innerText = barometricPressure;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
